45b4d02349271526bd39f40d985991d63ebbef65
Tiago Oliveira <94474189+Agotsilv@users.noreply.github.com>
2024-06-26 09:31:18 -0300

    feat: conexão com banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local.database'],
+      isGlobal: true, // Torna o ConfigModule globalmente disponível
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'mysecretpassword',
+      database: 'postgres',
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/migrations/*{.js,.ts}`],
+      migrationsRun: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
